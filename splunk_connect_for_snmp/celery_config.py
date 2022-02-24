7966077481bf26e012843960f490c6448f234952
omrozowicz-splunk <omrozowicz@splunk.com>
2022-02-24 14:52:07 +0100

    fix: implement x-consistent-hash solution for rabbitmq queue

--- splunk_connect_for_snmp/celery_config.py
+++ splunk_connect_for_snmp/celery_config.py
@@ -46,8 +46,44 @@ mongodb_scheduler_db = MONGO_DB_CELERY_DATABASE
 # Optimization for long running tasks
 # https://docs.celeryproject.org/en/stable/userguide/optimizing.html#reserve-one-task-at-a-time
 task_acks_late = True
-worker_prefetch_multiplier = 30
+worker_prefetch_multiplier = 1
 task_acks_on_failure_or_timeout = True
 task_reject_on_worker_lost = True
 task_time_limit = 2400
+task_create_missing_queues = False
 task_ignore_result = True
+result_persistent = False
+result_expires = 60
+default_exchange = Exchange('default', delivery_mode=1, type='x-consistent-hash')
+task_queues = (
+    Queue('traps', default_exchange, routing_key='1', durable=False),
+    Queue('traps1', default_exchange, routing_key='1', durable=False),
+)
+task_default_exchange = 'default'
+task_default_queue = 'traps1'
+task_routes = {
+    'splunk_connect_for_snmp.snmp.tasks.trap': {
+        'exchange': 'default',
+        'routing_key': '1'
+    },
+    'splunk_connect_for_snmp.splunk.tasks.send': {
+        'exchange': 'default',
+        'routing_key': '2',
+    },
+    'splunk_connect_for_snmp.splunk.tasks.prepare': {
+        'exchange': 'default',
+        'routing_key': '3'
+    },
+    'splunk_connect_for_snmp.enrich.tasks.enrich': {
+        'exchange': 'default',
+        'routing_key': '4',
+    },
+    'splunk_connect_for_snmp.inventory.tasks.inventory_setup_poller': {
+        'exchange': 'default',
+        'routing_key': '5'
+    },
+    'splunk_connect_for_snmp.snmp.tasks.poll': {
+        'exchange': 'default',
+        'routing_key': '6'
+    }
+}
