3247fba538976556a6e8c55c5ff5ea262002bfdc
Pawel Szkamruk <pszkamruk@splunk.com>
2023-07-21 14:35:13 +0200

    Poc v3

--- ui_tests/second.py
+++ ui_tests/second.py
@@ -9,21 +9,21 @@ def run_headless_selenium_test():
 
     driver = webdriver.Chrome(options=chrome_options)
 
-    url = "http://10.202.37.235:30001/"
+    # url = "http://10.202.37.235:30001/"
+    # driver.get(url)
+    #
+    # time.sleep(2)
+    # print("select inventory tab")
+    # inventory_tab = driver.find_element_by_css_selector("#main-component-container > div.sc-AxirZ.dGbZTB > div.sc-AxiKw.caSCzR > div > button:nth-child(3) > div.TabStyles__StyledLabel-sc-1ry8mzj-3.gRwQTe")
+    # inventory_tab.click()
+    # time.sleep(3)
+    # driver.quit()
+
+    url = "https://www.onet.pl/"
     driver.get(url)
+    page_title = driver.title
+    print("Page Title:", page_title)
 
-    time.sleep(2)
-    print("select inventory tab")
-    inventory_tab = driver.find_element_by_css_selector("#main-component-container > div.sc-AxirZ.dGbZTB > div.sc-AxiKw.caSCzR > div > button:nth-child(3) > div.TabStyles__StyledLabel-sc-1ry8mzj-3.gRwQTe")
-    inventory_tab.click()
-    time.sleep(3)
-    driver.quit()
-
-    print("click apply changes button")
-    # Find an element by its ID and perform an action (e.g., filling in a search field)
-    apply_button = driver.find_element_by_css_selector("#main-component-container > div.sc-AxhUy.biNwaT > div.sc-AxheI.hSSwJb > div > button.ClickableStyles__StyledA-sc-7al1vw-0.osAQV.ButtonSimpleStyles__StyledClickable-vlarwe-0.jOPXmR.ButtonStyles__StyledButtonSimple-eqxqs2-1.imMhPF > span > span")
-    apply_button.click()
-    time.sleep(12)
 
 if __name__ == "__main__":
     run_headless_selenium_test()
